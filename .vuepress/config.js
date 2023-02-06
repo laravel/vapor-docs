24a8bedb677c1893865f0d71fb14298fd450498c
tal7aouy <tal7aouy.dev@gmail.com>
2023-02-06 15:49:12 +0100

    added:go-to-top button

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,32 +1,27 @@
 module.exports = {
-  title: 'Laravel Vapor',
-  description: 'Laravel at Infinite Scale',
-
+  title: "Laravel Vapor",
+  description: "Laravel at Infinite Scale",
   head: [
     [
-      'link',
+      "link",
       {
-        href:
-          'https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i',
-        rel: 'stylesheet',
-        type: 'text/css',
+        href: "https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,800,800i,900,900i",
+        rel: "stylesheet",
+        type: "text/css",
       },
     ],
   ],
-
+  plugins: ["@vuepress/back-to-top"],
   themeConfig: {
     // logo: '/assets/img/logo.svg',
     displayAllHeaders: true,
     sidebarDepth: 1,
-    searchPlaceholder: 'Press / to search',
+    searchPlaceholder: "Press / to search",
 
-
-    nav: [
-      { text: 'Home', link: 'https://vapor.laravel.com' }
-    ],
+    nav: [{ text: "Home", link: "https://vapor.laravel.com" }],
 
     sidebar: {
-      '/1.0/': require('./1.0')
+      "/1.0/": require("./1.0"),
     },
   },
-}
+};
